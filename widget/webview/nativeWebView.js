51569f7ef53d76bc9fc3d0cbd0002cf98ac2d98e
Marc Plouhinec <m_plouhinec@yahoo.fr>
2013-07-20 14:43:46 +0200

    Add the functions nativeWebView.fireExternalEvent and removeView.

--- widget/webview/nativeWebView.js
+++ widget/webview/nativeWebView.js
@@ -14,6 +14,25 @@ define(function() {
          * @param {String} jsonLayoutParams JSON-serialized LayoutParams
          */
         'buildView': function(jsonLayoutParams) {
+        },
+
+        /**
+         * Fire an event to a listener that is outside of the WebView.
+         *
+         * @param {String} webViewPlaceHolderId
+         * @param {String} eventName
+         * @param {String} jsonPayload
+         */
+        'fireExternalEvent': function(webViewPlaceHolderId, eventName, jsonPayload) {
+        },
+
+        /**
+         * Remove the view with the given ID.
+         *
+         * @param {String} id
+         *     Place holder ID.
+         */
+        'removeView': function(id) {
         }
     };
 
